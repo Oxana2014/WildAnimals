a81e8842bc0dcc8a9c4fe84e409e50ef71d2f673
Oxana <keysb@mail.ru>
2022-03-24 16:59:21 +0400

    add start script

--- app.js
+++ app.js
@@ -113,7 +113,8 @@ app.use((err, req, res, next) => {
     if(!err.message) err.message = "Something went wrong!"
     res.status(statusCode).render('error', {err});
 })
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Serving on port 3000!");
+app.listen(port, () => {
+    console.log(`Serving on port ${port}!`);
 })
