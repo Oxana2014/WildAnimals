a9bcca47b0f3fe9b5b342a3f5e5fee7f8dc9408f
Oxana <keysb@mail.ru>
2022-03-25 14:19:59 +0400

    changed repository

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,6 +1,6 @@
 
-  //mapboxgl.accessToken = mapToken;
-  mapboxgl.accessToken = "pk.eyJ1Ijoid2lsZHktayIsImEiOiJjbDBseGV0amEwNXNsM2Jxejk2aWFiem9pIn0.POY-m41PLobogQidu2JHQQ"
+  mapboxgl.accessToken = mapToken;
+  // mapboxgl.accessToken = "pk.eyJ1Ijoid2lsZHktayIsImEiOiJjbDBseGV0amEwNXNsM2Jxejk2aWFiem9pIn0.POY-m41PLobogQidu2JHQQ"
 
   const map = new mapboxgl.Map({
   container: 'map', // container ID
